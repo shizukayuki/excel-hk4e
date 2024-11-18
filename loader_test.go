9790572f1f49912b070796995b89ddb436113181
Shizuka <shizuka.alpha@gmail.com>
2024-11-19 02:08:39 +0400

    Add tests for textmap merge

---
+++ loader_test.go
@@ -0,0 +1,73 @@
+package excel_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/shizukayuki/excel-hk4e"
+)
+
+func setup(t *testing.T, langs []string, override map[string]string) {
+	err := excel.LoadResources(excel.LoaderConfig{
+		Languages: langs,
+		ReadFile: func(root string, name string) ([]byte, error) {
+			data := []byte("[]")
+			if strings.HasPrefix(strings.ToLower(name), "textmap") {
+				if b, ok := override[name]; ok {
+					fmt.Printf("loaded %s...\n", name)
+					data = []byte(b)
+				} else {
+					fmt.Printf("trying %s...\n", name)
+				}
+			}
+			return data, nil
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestTextMapMerge(t *testing.T) {
+	override := map[string]string{
+		"TextMap/TextMapEN.json": `{
+			"1": "1",
+			"2": "2",
+			"3": "3"
+		}`,
+
+		"TextMap/TextMapJP.json":   `{ "1": "1" }`,
+		"TextMap/TextMapJP_1.json": `{ "2": "2" }`,
+		"TextMap/TextMapJP_2.json": `{ "3": "3" }`,
+
+		"TextMap/TextMapTH_1.json": `{ "1": "1" }`,
+		"TextMap/TextMapTH_2.json": `{ "2": "2" }`,
+	}
+	expected := map[string]map[excel.TextMapHash]string{
+		"en": {
+			1: "1",
+			2: "2",
+			3: "3",
+		},
+		"jp": {
+			1: "1",
+			2: "",
+			3: "",
+		},
+		"th": {
+			1: "1",
+			2: "2",
+			3: "",
+		},
+	}
+	setup(t, []string{"en", "jp", "th"}, override)
+
+	for lang, expected := range expected {
+		for h, v := range expected {
+			if h.Lang(lang) != v {
+				t.Errorf("lang=%s,hash=%d results in '%v'. expected '%v'", lang, h, h.Lang(lang), v)
+			}
+		}
+	}
+}
